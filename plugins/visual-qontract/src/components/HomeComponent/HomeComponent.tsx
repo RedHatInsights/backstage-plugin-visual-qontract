7e307b2f195536c9f64306f0e097a746f678c789
Adam Drew <addrew@redhat.com>
2024-05-23 09:19:19 -0400

    Getting started

--- plugins/visual-qontract/src/components/HomeComponent/HomeComponent.tsx
+++ plugins/visual-qontract/src/components/HomeComponent/HomeComponent.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Typography,
 } from '@material-ui/core';
+import {
+  useStarredEntities,
+  EntityRefLink,
+  EntityRefLinks,
+} from '@backstage/plugin-catalog-react'
 
 export const HomeComponent = () => {
+  const { starredEntities } = useStarredEntities();
+
+  useEffect(() => {
+    console.log(starredEntities);
+  }, [starredEntities]);
+
   return (
     <Typography variant="h6" color="textPrimary">
       Hello World
+      {
+        [...starredEntities].map((entity) => (
+          <EntityRefLink entityRef={entity} key={entity.toString()} />
+        ))
+      }
+      <EntityRefLinks entityRefs={[...starredEntities]} />
     </Typography>
   )
 };
