296b42599a35c7b5d82347eec6aab7cc9eafcabf
Adam Drew <addrew@redhat.com>
2024-08-01 08:16:13 -0400

    CodeComponent icons and styling

--- plugins/visual-qontract/src/components/CodeComponentsComponent/CodeComponentsComponent.tsx
+++ plugins/visual-qontract/src/components/CodeComponentsComponent/CodeComponentsComponent.tsx
@@ -1,120 +1,196 @@
 import React from 'react';
 import {
-    Typography,
-    IconButton,
-    Link,
-    Table,
-    TableContainer,
-    TableRow,
-    TableBody,
-    TableHead,
-    TableCell,
-    Paper
+  Typography,
+  Link,
+  Table,
+  TableContainer,
+  TableRow,
+  TableBody,
+  TableHead,
+  TableCell,
+  Paper,
+  Box,
 } from '@material-ui/core';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import NotListedLocationIcon from '@material-ui/icons/NotListedLocation'
-import {
-    InfoCard,
-} from '@backstage/core-components';
+import NotListedLocationIcon from '@material-ui/icons/NotListedLocation';
+import { InfoCard } from '@backstage/core-components';
 import { CodeComponentsQuery } from './query';
 import { RELATION_HAS_PART } from '@backstage/catalog-model';
 import { useEntity, useRelatedEntities } from '@backstage/plugin-catalog-react';
-import QueryQontract from '../../common/QueryAppInterface';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
+import QueryQontract from '../../common/QueryAppInterface';
+import Icon from '@mdi/react';
+import { mdiGitlab } from '@mdi/js';
+import { mdiPipe } from '@mdi/js';
+import { mdiApplicationExport } from '@mdi/js';
 
 export const CodeComponentsComponent = () => {
-    const title = "Code Repos & Build Jobs";
+  const title = 'Code Repos & Build Jobs';
 
-    const { entity } = useEntity();
+  const { entity } = useEntity();
 
-    const { entities: relatedEntities, loading: relatedEntitiesLoading, error: relatedEntitiesError } = useRelatedEntities(entity, {
-        type: RELATION_HAS_PART,
-        kind: "Component"
-    });
+  const {
+    entities: relatedEntities,
+    loading: relatedEntitiesLoading,
+    error: relatedEntitiesError,
+  } = useRelatedEntities(entity, {
+    type: RELATION_HAS_PART,
+    kind: 'Component',
+  });
 
-    const { result: qontractResult, loaded: qontractLoaded, error: qontractError } = QueryQontract(CodeComponentsQuery);
+  const {
+    result: qontractResult,
+    loaded: qontractLoaded,
+    error: qontractError,
+  } = QueryQontract(CodeComponentsQuery);
 
-    if (relatedEntitiesError || qontractError) {
-        return <InfoCard title={title}>
-            <Typography align="center" variant="body1">
-                Error loading the {title} information.
-            </Typography>
+  if (relatedEntitiesError || qontractError) {
+    return (
+      <InfoCard title={title}>
+        <Typography align="center" variant="body1">
+          Error loading the {title} information.
+        </Typography>
+      </InfoCard>
+    );
+  }
+
+  if (relatedEntitiesLoading || !qontractLoaded) {
+    return (
+      <InfoCard title={title}>
+        <Typography align="center" variant="body1">
+          Loading...
+        </Typography>
+      </InfoCard>
+    );
+  }
+
+  if (
+    qontractResult.apps_v1.length === 0 ||
+    qontractResult.apps_v1[0].codeComponents.length === 0
+  ) {
+    return (
+      <InfoCard title={title}>
+        <Typography align="center" variant="body1">
+          No {title} found.
+        </Typography>
+      </InfoCard>
+    );
+  }
+
+  const CodeComponentsTable = () => {
+    // For each code component, try to locate a child entity which has a matching source code URL.
+    // If a match is found, use that entity's 'image-build-url' annotation value if defined
+
+    if (!qontractResult.apps_v1[0].codeComponents) {
+      return (
+        <InfoCard title={title}>
+          <Typography align="center" variant="body1">
+            No {title} found.
+          </Typography>
         </InfoCard>
+      );
     }
 
-    if (relatedEntitiesLoading || !qontractLoaded) {
-        return <InfoCard title={title}>
-            <Typography align="center" variant="body1">
-                Loading...
-            </Typography>
-        </InfoCard>
-    }
-
-    if (qontractResult.apps_v1.length === 0 || qontractResult.apps_v1[0].codeComponents.length === 0 ) {
-        return (
-          <InfoCard title={title}>
-            <Typography align="center" variant="body1">No {title} found.</Typography>
-          </InfoCard>
-        );
-    }
-
-    const CodeComponentsTable = () => {
-        // For each code component, try to locate a child entity which has a matching source code URL.
-        // If a match is found, use that entity's 'image-build-url' annotation value if defined
-
-        if ( !qontractResult.apps_v1[0].codeComponents) {
-            return (
-                <InfoCard title={title}>
-                    <Typography align="center" variant="body1">No {title} found.</Typography>
-                </InfoCard>
-            );
-        }
-
-        if (relatedEntities) {
-            for (var component of qontractResult.apps_v1[0].codeComponents) {
-                for (var relatedEntity of relatedEntities) {
-                    if (relatedEntity.metadata.annotations?.["backstage.io/source-location"] == `url:${component.url}`) {
-                        var annotationBuildUrl = relatedEntity.metadata.annotations?.["visual-qontract/image-build-url"]
-                        if (annotationBuildUrl) {
-                            component.imageBuildUrl = annotationBuildUrl
-                        }
-                    }
-                }
+    if (relatedEntities) {
+      for (var component of qontractResult.apps_v1[0].codeComponents) {
+        for (var relatedEntity of relatedEntities) {
+          if (
+            relatedEntity.metadata.annotations?.[
+              'backstage.io/source-location'
+            ] == `url:${component.url}`
+          ) {
+            var annotationBuildUrl =
+              relatedEntity.metadata.annotations?.[
+                'visual-qontract/image-build-url'
+              ];
+            if (annotationBuildUrl) {
+              component.imageBuildUrl = annotationBuildUrl;
             }
+          }
         }
-
-        return  <TableContainer component={Paper}>
-                <Table size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell><Typography variant="button">Name</Typography></TableCell>
-                            <TableCell align="center"><Typography variant="button">Build Job</Typography></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {qontractResult.apps_v1[0].codeComponents?.map((component: any, key: any) => (
-                            <TableRow key={key}>
-                                <TableCell scope="row">
-                                    <Link target="_blank" href={component.url}>
-                                        {component.name}
-                                    </Link>
-                                </TableCell>
-                                <TableCell align="center">
-                                    <IconButton aria-label="go-to-build-job" size="small">
-                                        {component.imageBuildUrl
-                                            ? <Link target="_blank" href={component.imageBuildUrl}><ExitToAppIcon /></Link>
-                                            : <NotListedLocationIcon />
-                                        }
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+      }
     }
 
-    return <InfoCard title={title} noPadding>
-            <CodeComponentsTable />
+    const RepoIcon = (props: any) => {
+      if (props.url.includes('github.com')) {
+        return <GitHubIcon />;
+      } else {
+        return <Icon path={mdiGitlab} size={1} />;
+      }
+    };
+
+    console.log(qontractResult.apps_v1[0].codeComponents);
+
+    const RepoTableCell = (props: any) => {
+      const { component } = props;
+      return (
+        <TableCell scope="row">
+          <Box display="flex" alignItems="center">
+            <RepoIcon url={component.url} />
+            <Box ml={1}>
+              <Link target="_blank" href={component.url}>
+                {component.name}
+              </Link>
+            </Box>
+          </Box>
+        </TableCell>
+      );
+    };
+
+    const CodeComponentsTable = (props: any) => {
+      const { component } = props;
+      if (!component.imageBuildUrl) {
+        return (
+          <TableCell>
+            Not found.
+          </TableCell>
+        );
+      }
+      return (
+        <TableCell align="center">
+          <Box display="flex" alignItems="center">
+            <Icon path={mdiApplicationExport} size={1} />
+            <Box ml={1}>
+              <Link target="_blank" href={component.imageBuildUrl}>
+                {component.name}
+              </Link>
+            </Box>
+          </Box>
+        </TableCell>
+      );
+    };
+
+    return (
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="button">Name</Typography>
+              </TableCell>
+              <TableCell >
+                <Typography variant="button">Build Job</Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {qontractResult.apps_v1[0].codeComponents?.map(
+              (component: any, key: any) => (
+                <TableRow key={key}>
+                  <RepoTableCell component={component} />
+                  <CodeComponentsTable component={component} />
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
+  return (
+    <InfoCard title={title} noPadding>
+      <CodeComponentsTable />
     </InfoCard>
-}
+  );
+};
